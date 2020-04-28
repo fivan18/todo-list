ac745e914b346111d2491400719ea731f5fc3f99
Juan Pablo <jpablomgil@gmail.com>
2020-04-28 13:07:12 -0500

    Create module of local storage

--- src/local_storage.js
+++ src/local_storage.js
@@ -1,9 +1,17 @@
-function saveArr(arr) {
-  window.localStorage.setItem('library', JSON.stringify(arr));
-}
+var localStorage = (function() {
 
-function retrieveArr() {
-  let arr = [];
-  if (window.localStorage.getItem('library') !== null) arr = JSON.parse(window.localStorage.getItem('library'));
-  return arr;
-}
+  function saveArr(arr) {
+    window.localStorage.setItem('library', JSON.stringify(arr));
+  }
+
+  function retrieveArr() {
+    let arr = [];
+    if (window.localStorage.getItem('library') !== null) arr = JSON.parse(window.localStorage.getItem('library'));
+    return arr;
+  }
+
+  return  { retrieveArr, saveArr }
+
+})();
+
+export { localStorage }
