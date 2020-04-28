44a9fe34c00654cd716ca996bec47f7b339e9ab9
Ivan <fivanunam@gmail.com>
2020-04-28 15:28:25 -0500

    add basic logic to create a project

--- src/index.js
+++ src/index.js
@@ -2,9 +2,29 @@ import './assets/css/style.css';
 import './assets/css/sidebar.css';
 import { projectFactory } from './todo' ;
 import { localStorage } from './local_storage' ;
+import { dom } from './domManipulation'
+
+function validateStr(str, max, min = 0) {
+  if (str.length < max && str.length > min) {
+    return true;
+  }
+  return false;
+}
+
+function projectHandler(event){
+  const input = dom.getElement(document, '.project-input').value;
+  console.log(input);
+  if (validateStr(input, 20, 5)) {
+    const project = projectFactory(input);
+    console.log(project);
+  } else {
+    console.log('let user know that she/he needs to write a better name');
+  }
+}
+
+dom.setEventHandler('.project-button', 'click', projectHandler);
 
 
-const project = projectFactory('todo list app');
 
-
-project.addTodo('set repository');
+// ;
+// project.addTodo('set repository');
